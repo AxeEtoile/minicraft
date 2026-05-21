059af50dce06c8610b7a25074f8a7bad23e66b66
AxeEtoile <lavietheo08@gmail.com>
2026-05-21 19:50:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const http = require("http");
 const fs = require("fs");
+const WebSocket = require("ws");
+
+// ✅ ROOMS (étape 1)
+let rooms = {};
 
 const server = http.createServer((req, res) => {
   fs.readFile("minicraft_vmultiplayer.html", (err, data) => {
@@ -10,6 +14,56 @@ const server = http.createServer((req, res) => {
     }
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(data);
+  });
+});
+
+// ✅ WEBSOCKET
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (ws) => {
+  let roomCode = null;
+  let playerId = Math.random().toString(36).substr(2, 9);
+
+  ws.on("message", (message) => {
+    const data = JSON.parse(message);
+
+    // 🔑 rejoindre une room
+    if (data.type === "join") {
+      roomCode = data.room;
+
+      if (!rooms[roomCode]) {
+        rooms[roomCode] = {};
+      }
+
+      rooms[roomCode][playerId] = { x: 0, y: 0 };
+
+      ws.send(JSON.stringify({
+        type: "init",
+        id: playerId,
+        players: rooms[roomCode]
+      }));
+    }
+
+    // 🎮 mouvement
+    if (data.type === "move" && roomCode) {
+      rooms[roomCode][playerId] = data.position;
+
+      // envoyer aux joueurs de la room
+      wss.clients.forEach((client) => {
+        if (client.readyState === 1) {
+          client.send(JSON.stringify({
+            type: "update",
+            players: rooms[roomCode]
+          }));
+        }
+      });
+    }
+  });
+
+  ws.on("close", () => {
+    if (roomCode && rooms[roomCode]) {
+      delete rooms[roomCode][playerId];
+    }
   });
 });
 
